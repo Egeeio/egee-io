e0323773e84e3e3ecdb59a98cecdf4668ac0f1ad
Brian Warren <brian@egee.io>
2016-07-23 12:22:53 -0700

    removed some old files

--- tests/unit/routes/servers-test.js
+++ tests/unit/routes/servers-test.js
@@ -1,8 +1,6 @@
 import { moduleFor, test } from 'ember-qunit';
 
 moduleFor('route:servers', 'Unit | Route | servers', {
-  // Specify the other units that are required for this test.
-  // needs: ['controller:foo']
 });
 
 test('it exists', function(assert) {
