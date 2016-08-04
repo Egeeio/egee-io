b4e2fb69a5dbf4683dbab154fb47c652fee5a1b3
Brian Warren <brian@egee.io>
2016-08-04 14:18:36 -0700

    Began conversion from Materialize to EmberPaper

--- config/environment.js
+++ config/environment.js
@@ -11,12 +11,8 @@ module.exports = function(environment) {
     },
 
     APP: {
-      // Optional flags/options that we will never use
-    },
-    sassOptions: {
-      includePaths: ['bower_components/materialize/sass']
     }
-  };
+  }
 
   if (environment === 'development') {
     // ENV.APP.LOG_RESOLVER = true;
