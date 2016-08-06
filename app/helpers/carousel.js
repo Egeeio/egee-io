b97087f4ba66f96b40987f6b765efe6f17e19e57
Brian Warren <brian@egee.io>
2016-08-05 18:00:29 -0700

    Removed references to jquery

--- app/helpers/carousel.js
+++
@@ -1,9 +0,0 @@
-import Ember from 'ember';
-
-export function carousel() {
-  $(document).ready(function(){
-    $('.carousel').carousel();
-  });
-}
-
-export default Ember.Helper.helper(carousel);
