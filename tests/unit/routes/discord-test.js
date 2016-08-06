ed12f6363b7dbf07d9791667cda283b78700b8d3
Brian Warren <brian@egee.io>
2016-08-06 09:38:51 -0700

    Revert "removed some old files"
    This reverts commit e0323773e84e3e3ecdb59a98cecdf4668ac0f1ad.

--- tests/unit/routes/discord-test.js
+++ tests/unit/routes/discord-test.js
@@ -1,6 +1,6 @@
 import { moduleFor, test } from 'ember-qunit';
 
-moduleFor('route:servers', 'Unit | Route | servers', {
+moduleFor('route:discord', 'Unit | Route | discord', {
   // Specify the other units that are required for this test.
   // needs: ['controller:foo']
 });
