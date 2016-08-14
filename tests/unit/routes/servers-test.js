3d95059ea1f322634d17d30d096be6a1052ae4a5
Brian Warren <brian@egee.io>
2016-08-14 15:33:47 -0700

    More responsive changes

--- tests/unit/routes/servers-test.js
+++
@@ -1,9 +0,0 @@
-import { moduleFor, test } from 'ember-qunit';
-
-moduleFor('route:servers', 'Unit | Route | servers', {
-});
-
-test('it exists', function(assert) {
-  let route = this.subject();
-  assert.ok(route);
-});
