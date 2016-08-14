5a74396c3da56442cf84c5e038a58a19c19b967a
Brian Warren <brian@egee.io>
2016-08-14 15:48:25 -0700

    Disabled linting on build

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,9 +1,10 @@
 /* global require, module */
-var EmberApp = require('ember-cli/lib/broccoli/ember-app')
+const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
-module.exports = function(defaults) {
-  var app = new EmberApp(defaults, {
-  })
+module.exports = function (defaults) {
+  const app = new EmberApp(defaults, {
+    hinting: false,
+  });
 
-  return app.toTree()
-}
+  return app.toTree();
+};
