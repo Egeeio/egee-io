927e32c3ed77ebd661afad5d489500cce3d6bb20
egee-irl <egee-irl>
2016-08-14 22:03:51 -0700

    Configuration and prod

--- config/environment.js
+++ config/environment.js
@@ -28,11 +28,11 @@ module.exports = function (environment) {
   }
 
   if (environment === 'production') {
-    // ENV.APP.LOG_RESOLVER = true;
-    // ENV.APP.LOG_ACTIVE_GENERATION = true;
-    // ENV.APP.LOG_TRANSITIONS = true;
-    // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
-    // ENV.APP.LOG_VIEW_LOOKUPS = true;
+    ENV.APP.LOG_RESOLVER = false;
+    ENV.APP.LOG_ACTIVE_GENERATION = false;
+    ENV.APP.LOG_TRANSITIONS = false;
+    ENV.APP.LOG_TRANSITIONS_INTERNAL = false;
+    ENV.APP.LOG_VIEW_LOOKUPS = false;
   }
 
   return ENV;
