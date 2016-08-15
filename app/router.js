ea42f78638c7c23264e2a196259faea7441d28e5
Brian Warren <brian@egee.io>
2016-08-14 21:32:48 -0700

    Final changes before merge

--- app/router.js
+++ app/router.js
@@ -6,10 +6,11 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function () {
-  this.route('minecraft');
-  this.route('rust');
-  this.route('gmod');
-  this.route('terraria');
+  this.route('servers/minecraft', { path: '/minecraft' });
+  // this.route('minecraft');
+  this.route('servers/rust', { path: '/rust' });
+  this.route('servers/gmod', { path: '/gmod' });
+  this.route('servers/terraria', { path: '/terraria' });
   this.route('not-found', { path: '/*path' });
 });
 
