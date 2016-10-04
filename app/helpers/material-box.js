ee0dccfc1aa2e4a4194bdeb2be3904ef138d5482
egee-irl <brian@egee.io>
2016-10-04 11:13:10 -0700

    materialbox to materialslider

--- app/helpers/material-box.js
+++
@@ -1,9 +0,0 @@
-import Ember from 'ember';
-
-export function materialBox() {
-    $(document).ready(function(){
-    $('.materialboxed').materialbox();
-  });
-}
-
-export default Ember.Helper.helper(materialBox);
---
+++ app/helpers/material-slider.js
@@ -0,0 +1,9 @@
+import Ember from 'ember';
+
+export function materialSlider() {
+    $(document).ready(function(){
+      $('.slider').slider({full_width: true});
+    });
+}
+
+export default Ember.Helper.helper(materialSlider);
