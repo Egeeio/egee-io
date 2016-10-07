08eeff7f956483ebdd117e26e7015c648aa975c4
egee-irl <brian@egee.io>
2016-10-07 13:27:02 -0700

    persona images

---
+++ app/helpers/persona-name.js
@@ -0,0 +1,8 @@
+import Ember from 'ember';
+
+export function personaName(name) {
+  const normalizeName = name.pop();
+  return 'Game Title is missing from game-title.js file';
+}
+
+export default Ember.Helper.helper(personaName);
