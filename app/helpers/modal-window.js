bb1670de1820d71eabc11c3201c6c11e1fe162c4
egee-irl <brian@egee.io>
2016-10-09 00:16:46 -0700

    community dialog modal

--- app/helpers/modal-window.js
+++ app/helpers/modal-window.js
@@ -4,44 +4,44 @@ export function modalWindow(name) {
   $(document).ready(() => {
     const normalizeName = name.pop();
     let dialog;
-    let showDialogButton;
+    let personaLink;
 
-    // if (! dialog.showModal) {
-    //   dialogPolyfill.registerDialog(dialog);
-    // }
     switch (normalizeName) {
-      case 'egee':
-        dialog = document.querySelector('.egee_dialog');
-        showDialogButton = document.querySelector('#egee_dialog');
-        break;
       case 'monkey':
         dialog = document.querySelector('.monkey_dialog');
-        showDialogButton = document.querySelector('#monkey_dialog');
+        personaLink = document.querySelector('#monkey_dialog');
         break;
       case 'ghost':
         dialog = document.querySelector('.ghost_dialog');
-        showDialogButton = document.querySelector('#ghost_dialog');
+        personaLink = document.querySelector('#ghost_dialog');
         break;
       case 'omega':
         dialog = document.querySelector('.omega_dialog');
-        showDialogButton = document.querySelector('#omega_dialog');
+        personaLink = document.querySelector('#omega_dialog');
         break;
       case 'rice':
         dialog = document.querySelector('.rice_dialog');
-        showDialogButton = document.querySelector('#rice_dialog');
+        personaLink = document.querySelector('#rice_dialog');
         break;
       case 'teal':
         dialog = document.querySelector('.teal_dialog');
-        showDialogButton = document.querySelector('#teal_dialog');
+        personaLink = document.querySelector('#teal_dialog');
         break;
       case 'sudo':
         dialog = document.querySelector('.sudo_dialog');
-        showDialogButton = document.querySelector('#sudo_dialog');
+        personaLink = document.querySelector('#sudo_dialog');
         break;
       default:
         return;
     }
-    showDialogButton.addEventListener('click', () => dialog.showModal());
+    if (!dialog.showModal) {
+      dialogPolyfill.registerDialog(dialog);
+    }
+    personaLink.addEventListener('click', () => dialog.showModal());
+
+    dialog.querySelector('.close').addEventListener('click', () => {
+      dialog.close();
+    });
   });
 }
 
