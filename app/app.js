76b99f6d0a129c22262464938d6189c14ae43d14
egee-irl <brian@egee.io>
2016-10-28 14:36:52 -0700

    init

--- app/app.js
+++ app/app.js
@@ -1,15 +1,18 @@
-import Ember from 'ember'
-import loadInitializers from 'ember-load-initializers'
-import Resolver from './resolver'
-import config from './config/environment'
+import Ember from 'ember';
+import Resolver from './resolver';
+import loadInitializers from 'ember-load-initializers';
+import config from './config/environment';
 
-Ember.MODEL_FACTORY_INJECTIONS = true
-const App = Ember.Application.extend({
+let App;
+
+Ember.MODEL_FACTORY_INJECTIONS = true;
+
+App = Ember.Application.extend({
   modulePrefix: config.modulePrefix,
   podModulePrefix: config.podModulePrefix,
   Resolver
-})
+});
 
-loadInitializers(App, config.modulePrefix)
+loadInitializers(App, config.modulePrefix);
 
-export default App
+export default App;
--- config/environment.js
+++ config/environment.js
@@ -1,12 +1,26 @@
-module.exports = function (environment) {
-  const ENV = {
+/* jshint node: true */
+
+module.exports = function(environment) {
+  var ENV = {
     modulePrefix: 'egeeio',
-    environment,
-    baseURL: '/',
+    environment: environment,
+    rootURL: '/',
     locationType: 'auto',
-    APP: {
-      // This has to be here otherwise Ember will violently explode
+    EmberENV: {
+      FEATURES: {
+        // Here you can enable experimental features on an ember canary build
+        // e.g. 'with-controller': true
+      },
+      EXTEND_PROTOTYPES: {
+        // Prevent Ember Data from overriding Date.parse.
+        Date: false
+      }
     },
+
+    APP: {
+      // Here you can pass flags/options to your application instance
+      // when it is created
+    }
   };
 
   if (environment === 'development') {
@@ -18,9 +32,10 @@ module.exports = function (environment) {
   }
 
   if (environment === 'test') {
-    ENV.baseURL = '/';
+    // Testem prefers this...
     ENV.locationType = 'none';
 
+    // keep test console output quieter
     ENV.APP.LOG_ACTIVE_GENERATION = false;
     ENV.APP.LOG_VIEW_LOOKUPS = false;
 
@@ -28,11 +43,7 @@ module.exports = function (environment) {
   }
 
   if (environment === 'production') {
-    ENV.APP.LOG_RESOLVER = false;
-    ENV.APP.LOG_ACTIVE_GENERATION = false;
-    ENV.APP.LOG_TRANSITIONS = false;
-    ENV.APP.LOG_TRANSITIONS_INTERNAL = false;
-    ENV.APP.LOG_VIEW_LOOKUPS = false;
+
   }
 
   return ENV;
