2a89708b3bd5c4131fd2784394c57444890b6c70
egee-irl <brian@egee.io>
2016-12-23 07:31:24 -0800

    modals are rad

--- app/helpers/parallax.js
+++ app/helpers/parallax.js
@@ -1,8 +1,8 @@
 import Ember from 'ember';
 
 export function parallax() {
-    $(document).ready(function(){
-      $('.parallax').parallax();
+  $(document).ready(function(){
+    $('.parallax').parallax();
   });
 }
 
