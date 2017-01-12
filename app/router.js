b49b489d080410771520ef148f3a313db734f628
egee-irl <brian@egee.io>
2017-01-12 14:41:33 -0800

    lint all the things!

--- app/router.js
+++ app/router.js
@@ -1,12 +1,12 @@
-import Ember from 'ember';
-import config from './config/environment';
+import Ember from 'ember'
+import config from './config/environment'
 
 const Router = Ember.Router.extend({
   location: config.locationType
-});
+})
 
 Router.map(function() {
-  this.route('not-found', { path: '/*path' });
-});
+  this.route('not-found', { path: '/*path' })
+})
 
-export default Router;
+export default Router
--- config/environment.js
+++ config/environment.js
@@ -10,20 +10,20 @@ module.exports = function(environment) {
     },
 
     APP: { }
-  };
+  }
 
   if (environment === 'development') { }
 
   if (environment === 'test') {
-    ENV.baseURL = '/';
-    ENV.locationType = 'none';
+    ENV.baseURL = '/'
+    ENV.locationType = 'none'
 
-    ENV.APP.LOG_ACTIVE_GENERATION = false;
-    ENV.APP.LOG_VIEW_LOOKUPS = false;
+    ENV.APP.LOG_ACTIVE_GENERATION = false
+    ENV.APP.LOG_VIEW_LOOKUPS = false
 
-    ENV.APP.rootElement = '#ember-testing';
+    ENV.APP.rootElement = '#ember-testing'
   }
 
   if (environment === 'production') { }
-  return ENV;
-};
+  return ENV
+}
