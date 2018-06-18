6f80a8fda9958f51c38ea32c7fb98807f9c314ee
egee-irl <brian@egee.io>
2018-06-17 17:22:45 -0700

    purged old middleman stuff

--- app/helpers/carousel.js
+++
@@ -1,9 +0,0 @@
-import Ember from 'ember'
-
-export function carousel () {
-  $(document).ready(() => {
-    $('.carousel').carousel()
-  })
-}
-
-export default Ember.Helper.helper(carousel)
--- app/helpers/parallax.js
+++
@@ -1,9 +0,0 @@
-import Ember from 'ember'
-
-export function parallax () {
-  $(document).ready(() => {
-    $('.parallax').parallax()
-  })
-}
-
-export default Ember.Helper.helper(parallax)
