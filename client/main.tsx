3aab4803400bf2c52cd9da7bc2b715e398c03ab2
egee-irl <brian@egee.io>
2018-08-15 22:04:55 -0700

    tslint'd

--- client/main.tsx
+++ client/main.tsx
@@ -1,8 +1,8 @@
-import * as React from 'react'
-import { render } from 'react-dom'
-import { Meteor } from 'meteor/meteor'
-import App from '../imports/ui/app'
+import { Meteor } from "meteor/meteor";
+import * as React from "react";
+import { render } from "react-dom";
+import App from "../imports/ui/app";
 
 Meteor.startup(() => {
-  render(<App />, document.getElementById('root'))
-})
+  render(<App />, document.getElementById("root"));
+});
--- imports/ui/app.tsx
+++ imports/ui/app.tsx
@@ -1,9 +1,9 @@
-import * as React from 'react'
+import * as React from "react";
 
 export default class HelloWorld extends React.Component {
-  render () {
+  public render() {
     return (
       <h1>Hello World</h1>
-    )
+    );
   }
 }
