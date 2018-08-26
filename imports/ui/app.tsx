b10d76a41210c27fdb80f0eea427381ee109482b
egee-irl <brian@egee.io>
2018-08-26 10:05:32 -0700

    Now with proper CSS

--- imports/ui/app.tsx
+++ imports/ui/app.tsx
@@ -3,7 +3,7 @@ import * as React from "react";
 export default class HelloWorld extends React.Component {
   public render() {
     return (
-      <h1>Hello World</h1>
+      <p>Hello World</p>
     );
   }
 }
