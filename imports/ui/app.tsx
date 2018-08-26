eb6b410e72f6caf9f077035ccd4ecf47d1532505
egee-irl <brian@egee.io>
2018-08-26 10:45:19 -0700

    tslint-ified

--- imports/ui/app.tsx
+++ imports/ui/app.tsx
@@ -3,7 +3,21 @@ import * as React from "react";
 export default class HelloWorld extends React.Component {
   public render() {
     return (
-      <p>Hello World</p>
+      <section className="hero eg-blue-black is-large">
+        <div className="hero-body">
+          <div className="container">
+            <h1 className="title has-text-white-bis">
+              Egee.io
+            </h1>
+            <h2 className="subtitle has-text-white-bis">
+              Be different. Play together.
+            </h2>
+            <form>
+              <button className="button is-white is-outlined" formAction="/Servers">Our Servers</button>
+            </form>
+          </div>
+        </div>
+      </section>
     );
   }
 }
