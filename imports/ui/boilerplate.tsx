768e846f4b7df6b6c048d64d669407710df36c43
egee-irl <brian@egee.io>
2018-08-27 07:13:31 -0700

    added boilerplate ref code

---
+++ imports/ui/boilerplate.tsx
@@ -0,0 +1,12 @@
+// This class functionally does nothing
+// It serves as reference code when creating a new component
+
+import * as React from "react";
+
+export default class ChangeMe extends React.Component {
+  public render() {
+    return (
+      <section className=""/>
+    );
+  }
+}
