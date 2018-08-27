2b5a729e4450b93da984dd78c72725d8e021a56f
egee-irl <brian@egee.io>
2018-08-27 09:04:21 -0700

    added basic router

--- client/main.tsx
+++ client/main.tsx
@@ -1,8 +1,15 @@
+import createBrowserHistory from "history/createBrowserHistory";
 import { Meteor } from "meteor/meteor";
 import * as React from "react";
 import { render } from "react-dom";
+import { Route, Router } from "react-router";
 import App from "../imports/ui/app";
 
+const browserHistory = createBrowserHistory();
+
 Meteor.startup(() => {
-  render(<App />, document.getElementById("root"));
+  render(
+    <Router history={browserHistory}>
+      <Route path="/" component={App}/>
+    </Router>, document.getElementById("root"));
 });
