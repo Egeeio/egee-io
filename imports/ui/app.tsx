b2abc90c1969ce0277e86e5eb4472547230b5453
egee-irl <brian@egee.io>
2018-08-27 08:04:00 -0700

    cant have nested bodies

--- imports/ui/app.tsx
+++ imports/ui/app.tsx
@@ -2,16 +2,18 @@ import * as React from "react";
 import "./app.less";
 import Footer from "./footer";
 import IndexHero from "./indexHero";
+import IndexOSection from "./indexOSection";
 import NavBar from "./nav";
 
 export default class HelloWorld extends React.Component {
   public render() {
     return (
-      <body>
+      <div>
         <NavBar/>
         <IndexHero/>
+        <IndexOSection/>
         <Footer/>
-      </body>
+      </div>
     );
   }
 }
