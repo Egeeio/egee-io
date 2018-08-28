a1bf4cd323ea90b60e4757e2fa906e5b1efe4c94
egee-irl <brian@egee.io>
2018-08-27 21:12:44 -0700

    added router, index

--- client/main.tsx
+++ client/main.tsx
@@ -1,15 +1,8 @@
-import createBrowserHistory from "history/createBrowserHistory";
 import { Meteor } from "meteor/meteor";
 import * as React from "react";
 import { render } from "react-dom";
-import { Route, Router } from "react-router";
 import App from "../imports/ui/app";
 
-const browserHistory = createBrowserHistory();
-
 Meteor.startup(() => {
-  render(
-    <Router history={browserHistory}>
-      <Route path="/" component={App}/>
-    </Router>, document.getElementById("root"));
+  render(<App/>, document.getElementById("root"));
 });
---
+++ imports/ui/index.tsx
@@ -0,0 +1,15 @@
+import IndexHero from "./indexHero";
+import IndexOSection from "./indexOSection";
+
+import * as React from "react";
+
+export default class ChangeMe extends React.Component {
+  public render() {
+    return (
+      <div>
+        <IndexHero/>
+        <IndexOSection/>
+      </div>
+    );
+  }
+}
