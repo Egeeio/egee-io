bf36c679f2fc14a9b281f51709d17fb32c16c106
egee-irl <brian@egee.io>
2018-09-08 23:25:57 -0700

    linted & added bulma

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,10 +1,11 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import Helmet from 'react-helmet'
-import { StaticQuery, graphql } from 'gatsby'
+import React from "react"
+import PropTypes from "prop-types"
+import Helmet from "react-helmet"
+import { StaticQuery, graphql } from "gatsby"
 
-import Header from './header'
-import './layout.css'
+import Header from "./header"
+import "bulma/css/bulma.css"
+import "./layout.css"
 
 const Layout = ({ children }) => (
   <StaticQuery
@@ -22,8 +23,8 @@ const Layout = ({ children }) => (
         <Helmet
           title={data.site.siteMetadata.title}
           meta={[
-            { name: 'description', content: 'Sample' },
-            { name: 'keywords', content: 'sample, something' },
+            { name: "description", content: "Sample" },
+            { name: "keywords", content: "sample, something" },
           ]}
         >
           <html lang="en" />
