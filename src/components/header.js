28f41331d7984cdb8246cff5e20e0e8c8170723b
egee-irl <brian@egee.io>
2018-09-08 22:20:41 -0700

    removed styling

--- src/components/header.js
+++ src/components/header.js
@@ -2,27 +2,10 @@ import React from 'react'
 import { Link } from 'gatsby'
 
 const Header = ({ siteTitle }) => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
+  <div>
+    <div>
+      <h1>
+        <Link to="/">
           {siteTitle}
         </Link>
       </h1>
