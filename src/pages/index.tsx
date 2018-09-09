bd6579dd7ea9574792ed5aba3c01486537c5c178
egee-irl <brian@egee.io>
2018-09-09 08:51:50 -0700

    update paths

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
-import React from "react";
-import Hero from "../components/hero";
-import OsSection from "../components/osSection";
+import * as React from "react";
+import Hero from "../components/index/hero";
+import OsSection from "../components/index/osSection";
 import Layout from "../components/layout";
 
 export default class IndexPage extends React.Component {
