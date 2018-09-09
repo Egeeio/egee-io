9693b885472f4179b2750badf41724f7640dce0a
egee-irl <brian@egee.io>
2018-09-08 18:06:33 -0700

    removed meteor nonsense

--- client/main.tsx
+++
@@ -1,8 +0,0 @@
-import { Meteor } from "meteor/meteor";
-import * as React from "react";
-import { render } from "react-dom";
-import App from "../imports/ui/app";
-
-Meteor.startup(() => {
-  render(<App/>, document.getElementById("root"));
-});
