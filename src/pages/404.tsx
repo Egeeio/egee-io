4fa1afbfc2b8be070a55b673732d5831c40dcbf4
egee-irl <brian@egee.io>
2018-09-09 23:08:40 -0700

    seems fine to me

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -3,8 +3,8 @@ import Layout from "../components/layout";
 
 const NotFoundPage = () => (
   <Layout>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <h2>NOT FOUND</h2>
+    <p>You just hit a route that doesn&#39;t exist... Yes, I know the styling is broken. I'll fix it later.</p>
   </Layout>
 )
 
