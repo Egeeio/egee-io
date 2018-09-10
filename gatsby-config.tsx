22cd6553078b8b9a6a777ab1d4fe0143f6d4a7bc
egee-irl <brian@egee.io>
2018-09-09 21:40:42 -0700

    made gatsby-config ts

---
+++ gatsby-config.tsx
@@ -0,0 +1,11 @@
+module.exports = {
+  siteMetadata: {
+    title: "Egee.io | Be different. Play Together",
+    siteUrl: "https://egee.io",
+    description: "An alternative Linux & Windows Gaming Community"
+  },
+  plugins: [
+    "gatsby-plugin-typescript",
+    "gatsby-plugin-react-helmet",
+  ],
+}
