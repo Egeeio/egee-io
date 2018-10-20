8bdf6e5394c707d76e92a718dcd4ef1003092e2e
egee-irl <brian@egee.io>
2018-10-20 01:31:45 -0700

    replaced gmod with minecraft

--- imports/ui/servers/games.tsx
+++ imports/ui/servers/games.tsx
@@ -27,15 +27,15 @@ export default class Games extends React.Component {
               <div className="card">
                 <div className="card-image">
                   <figure className="image is-4by3">
-                    <img src="/images/games/gmod4by3.jpg" alt="Placeholder image"/>
+                    <img src="/images/games/tf24by3.jpg" alt="Placeholder image"/>
                   </figure>
                 </div>
                 <div className="card-content">
                   <div className="content">
-                    <h4>Egee & Create</h4>
+                  <h4>Egee & Gibs</h4>
                     <span><b>Server Rules</b></span><br/>
-                    <span className="card-rules">1) Absolutely <b>no</b> cheating (ESP in PropHunt, etc)</span><br/>
-                    <span className="card-rules">2) Do not try to crash the server (It's temping, I know)</span>
+                    <span className="card-rules">1) Play Nice.</span><br/>
+                    <span className="card-rules">2) Have Fun.</span>
                   </div>
                 </div>
               </div>
@@ -44,15 +44,15 @@ export default class Games extends React.Component {
               <div className="card">
                 <div className="card-image">
                   <figure className="image is-4by3">
-                    <img src="/images/games/openspades4by3.jpg" alt="Placeholder image"/>
+                    <img src="/images/games/minecraft4by3.jpg" alt="Placeholder image"/>
                   </figure>
                 </div>
                 <div className="card-content">
                   <div className="content">
-                  <h4>Egee & Shoot</h4>
+                    <h4>Egee & Build</h4>
                     <span><b>Server Rules</b></span><br/>
-                    <span className="card-rules">1) Play Nice.</span><br/>
-                    <span className="card-rules">2) Have Fun.</span>
+                    <span className="card-rules">1) Absolutely <b>no</b> cheating (ESP in PropHunt, etc)</span><br/>
+                    <span className="card-rules">2) Do not try to crash the server (It's temping, I know)</span>
                   </div>
                 </div>
               </div>
